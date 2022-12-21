070c512d66fad4f3dfaad307e5705e12fa92b396
evanc0 <93871370+evanc0@users.noreply.github.com>
2022-12-21 21:49:52 +0300

    upd 0.5

--- js/main.js
+++ js/main.js
@@ -7,3 +7,23 @@ $('.single-item').slick({
     arrows: false,
 
 });
+
+const menuBtn = document.querySelector('.top__burger');
+const menu = document.querySelector('.top__menu');
+const body = document.querySelector('body');
+const links = document.querySelectorAll('.top__wrapper_menu a');
+console.log(links);
+
+menuBtn.addEventListener('click', function() {
+    menuBtn.classList.toggle('active');
+    menu.classList.toggle('active');
+    body.classList.toggle('active');
+})
+
+links.forEach(function(link) {
+    link.addEventListener('click', function() {
+        menuBtn.classList.toggle('active');
+        menu.classList.toggle('active');
+        body.classList.toggle('active');
+    })
+})
